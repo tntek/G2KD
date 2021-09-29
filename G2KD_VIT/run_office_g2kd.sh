01808c96c7aa2df7e2400958806f4639ab98d6ac
TNTekLab <steventangsong@gmail.com>
2021-09-29 20:46:26 +0800

    Add files via upload

---
+++ G2KD_VIT/run_office_g2kd.sh
@@ -0,0 +1,99 @@
+########R1-------------------------------------
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 0 --t 1 --output_src ckpskdr0/source/ --output ckpskdr0/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 0 --t 2 --output_src ckpskdr0/source/ --output ckpskdr0/target_g2kd_0818/;
+
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 1 --t 0 --output_src ckpskdr0/source/ --output ckpskdr0/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 1 --t 2 --output_src ckpskdr0/source/ --output ckpskdr0/target_g2kd_0818/;
+
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 2 --t 0 --output_src ckpskdr0/source/ --output ckpskdr0/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 2 --t 1 --output_src ckpskdr0/source/ --output ckpskdr0/target_g2kd_0818/;
+
+########R1-------------------------------------
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 0 --t 1 --output_src ckpskdr1/source/ --output ckpskdr1/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 0 --t 2 --output_src ckpskdr1/source/ --output ckpskdr1/target_g2kd_0818/;
+
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 1 --t 0 --output_src ckpskdr1/source/ --output ckpskdr1/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 1 --t 2 --output_src ckpskdr1/source/ --output ckpskdr1/target_g2kd_0818/;
+
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 2 --t 0 --output_src ckpskdr1/source/ --output ckpskdr1/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 2 --t 1 --output_src ckpskdr1/source/ --output ckpskdr1/target_g2kd_0818/;
+
+########R2-------------------------------------
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 0 --t 1 --output_src ckpskdr2/source/ --output ckpskdr2/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 0 --t 2 --output_src ckpskdr2/source/ --output ckpskdr2/target_g2kd_0818/;
+
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 1 --t 0 --output_src ckpskdr2/source/ --output ckpskdr2/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 1 --t 2 --output_src ckpskdr2/source/ --output ckpskdr2/target_g2kd_0818/;
+
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 2 --t 0 --output_src ckpskdr2/source/ --output ckpskdr2/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 2 --t 1 --output_src ckpskdr2/source/ --output ckpskdr2/target_g2kd_0818/;
+
+########R3-------------------------------------
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 0 --t 1 --output_src ckpskdr3/source/ --output ckpskdr3/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 0 --t 2 --output_src ckpskdr3/source/ --output ckpskdr3/target_g2kd_0818/;
+
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 1 --t 0 --output_src ckpskdr3/source/ --output ckpskdr3/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 1 --t 2 --output_src ckpskdr3/source/ --output ckpskdr3/target_g2kd_0818/;
+
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 2 --t 0 --output_src ckpskdr3/source/ --output ckpskdr3/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 2 --t 1 --output_src ckpskdr3/source/ --output ckpskdr3/target_g2kd_0818/;
+
+########R4-------------------------------------
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 0 --t 1 --output_src ckpskdr4/source/ --output ckpskdr4/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 0 --t 2 --output_src ckpskdr4/source/ --output ckpskdr4/target_g2kd_0818/;
+
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 1 --t 0 --output_src ckpskdr4/source/ --output ckpskdr4/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 1 --t 2 --output_src ckpskdr4/source/ --output ckpskdr4/target_g2kd_0818/;
+
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 2 --t 0 --output_src ckpskdr4/source/ --output ckpskdr4/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 2 --t 1 --output_src ckpskdr4/source/ --output ckpskdr4/target_g2kd_0818/;
+
+########R5-------------------------------------
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 0 --t 1 --output_src ckpskdr5/source/ --output ckpskdr5/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 0 --t 2 --output_src ckpskdr5/source/ --output ckpskdr5/target_g2kd_0818/;
+
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 1 --t 0 --output_src ckpskdr5/source/ --output ckpskdr5/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 1 --t 2 --output_src ckpskdr5/source/ --output ckpskdr5/target_g2kd_0818/;
+
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 2 --t 0 --output_src ckpskdr5/source/ --output ckpskdr5/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 2 --t 1 --output_src ckpskdr5/source/ --output ckpskdr5/target_g2kd_0818/;
+
+########R6-------------------------------------
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 0 --t 1 --output_src ckpskdr6/source/ --output ckpskdr6/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 0 --t 2 --output_src ckpskdr6/source/ --output ckpskdr6/target_g2kd_0818/;
+
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 1 --t 0 --output_src ckpskdr6/source/ --output ckpskdr6/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 1 --t 2 --output_src ckpskdr6/source/ --output ckpskdr6/target_g2kd_0818/;
+
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 2 --t 0 --output_src ckpskdr6/source/ --output ckpskdr6/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 2 --t 1 --output_src ckpskdr6/source/ --output ckpskdr6/target_g2kd_0818/;
+
+########R7-------------------------------------
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 0 --t 1 --output_src ckpskdr7/source/ --output ckpskdr7/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 0 --t 2 --output_src ckpskdr7/source/ --output ckpskdr7/target_g2kd_0818/;
+
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 1 --t 0 --output_src ckpskdr7/source/ --output ckpskdr7/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 1 --t 2 --output_src ckpskdr7/source/ --output ckpskdr7/target_g2kd_0818/;
+
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 2 --t 0 --output_src ckpskdr7/source/ --output ckpskdr7/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 2 --t 1 --output_src ckpskdr7/source/ --output ckpskdr7/target_g2kd_0818/;
+
+########R8-------------------------------------
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 0 --t 1 --output_src ckpskdr8/source/ --output ckpskdr8/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 0 --t 2 --output_src ckpskdr8/source/ --output ckpskdr8/target_g2kd_0818/;
+
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 1 --t 0 --output_src ckpskdr8/source/ --output ckpskdr8/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 1 --t 2 --output_src ckpskdr8/source/ --output ckpskdr8/target_g2kd_0818/;
+
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 2 --t 0 --output_src ckpskdr8/source/ --output ckpskdr8/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 2 --t 1 --output_src ckpskdr8/source/ --output ckpskdr8/target_g2kd_0818/;
+
+########R9-------------------------------------
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 0 --t 1 --output_src ckpskdr9/source/ --output ckpskdr9/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 0 --t 2 --output_src ckpskdr9/source/ --output ckpskdr9/target_g2kd_0818/;
+
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 1 --t 0 --output_src ckpskdr9/source/ --output ckpskdr9/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 1 --t 2 --output_src ckpskdr9/source/ --output ckpskdr9/target_g2kd_0818/;
+
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 2 --t 0 --output_src ckpskdr9/source/ --output ckpskdr9/target_g2kd_0818/;
+~/anaconda3/bin/python target_g2kd.py --cls_par 0.05 --da uda --dset office --gpu_id 0 --s 2 --t 1 --output_src ckpskdr9/source/ --output ckpskdr9/target_g2kd_0818/;
---
+++ G2KD_VIT/run_office_home_g2kd_source.sh
@@ -0,0 +1,60 @@
+
+########R0-------------------------------------
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr0/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 0;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr0/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 1;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr0/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 2;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr0/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 3;
+
+########R1-------------------------------------
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr1/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 0;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr1/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 1;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr1/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 2;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr1/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 3;
+
+#######R2-------------------------------------
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr2/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 0;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr2/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 1;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr2/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 2;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr2/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 3;
+
+########R3-------------------------------------
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr3/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 0;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr3/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 1;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr3/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 2;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr3/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 3;
+
+########R4-------------------------------------
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr4/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 0;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr4/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 1;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr4/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 2;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr4/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 3;
+
+########R5-------------------------------------
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr5/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 0;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr5/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 1;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr5/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 2;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr5/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 3;
+
+########R6-------------------------------------
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr6/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 0;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr6/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 1;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr6/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 2;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr6/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 3;
+
+########R7-------------------------------------
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr7/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 0;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr7/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 1;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr7/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 2;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr7/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 3;
+
+########R8-------------------------------------
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr8/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 0;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr8/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 1;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr8/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 2;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr8/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 3;
+
+########R9-------------------------------------
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr9/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 0;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr9/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 1;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr9/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 2;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr9/source/ --da uda --gpu_id 0 --dset office-home --max_epoch 100 --s 3;
