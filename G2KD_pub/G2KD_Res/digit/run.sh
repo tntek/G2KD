28bfc5420b319a1ab1ce6d0374906f3bf0b93c36
TNTekLab <steventangsong@gmail.com>
2021-09-29 20:48:00 +0800

    Delete G2KD_pub directory

--- G2KD_pub/G2KD_Res/digit/run.sh
+++
@@ -1,4 +0,0 @@
-~/anaconda3/bin/python target_g2kd.py --dset m2u --gpu_id 0 --cls_par 0.01 --output ckps_digits_g2kd_r0
-~/anaconda3/bin/python target_g2kd.py --dset u2m --gpu_id 0 --cls_par 0.01 --output ckps_digits_g2kd_r0
-~/anaconda3/bin/python target_g2kd.py --dset s2m --gpu_id 0 --cls_par 0.01 --output ckps_digits_g2kd_r0
-
