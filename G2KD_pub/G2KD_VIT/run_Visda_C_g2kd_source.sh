412d8d5daf515e3d3b75bb759833feb8e27b645e
TNTekLab <steventangsong@gmail.com>
2021-09-29 20:39:25 +0800

    Add files via upload

---
+++ G2KD_pub/G2KD_VIT/run_Visda_C_g2kd_source.sh
@@ -0,0 +1,11 @@
+##################VC------------------------
+# ~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr0/source/ --da uda --gpu_id 0 --dset VISDA-C --lr 1e-3 --max_epoch 10 --s 0;
+# ~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr1/source/ --da uda --gpu_id 0 --dset VISDA-C --lr 1e-3 --max_epoch 10 --s 0;
+# ~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr2/source/ --da uda --gpu_id 0 --dset VISDA-C --lr 1e-3 --max_epoch 10 --s 0;
+# ~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr3/source/ --da uda --gpu_id 0 --dset VISDA-C --lr 1e-3 --max_epoch 10 --s 0;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr4/source/ --da uda --gpu_id 0 --dset VISDA-C --lr 1e-3 --max_epoch 10 --s 0;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr5/source/ --da uda --gpu_id 0 --dset VISDA-C --lr 1e-3 --max_epoch 10 --s 0;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr6/source/ --da uda --gpu_id 0 --dset VISDA-C --lr 1e-3 --max_epoch 10 --s 0;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr7/source/ --da uda --gpu_id 0 --dset VISDA-C --lr 1e-3 --max_epoch 10 --s 0;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr8/source/ --da uda --gpu_id 0 --dset VISDA-C --lr 1e-3 --max_epoch 10 --s 0;
+~/anaconda3/bin/python g2kd_source.py --trte val --output ckpskdr9/source/ --da uda --gpu_id 0 --dset VISDA-C --lr 1e-3 --max_epoch 10 --s 0;
